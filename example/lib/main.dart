df2b97f621427a97aed5dff8a4cbc4f56a231469
Caique-P <caiqueponjjar@gmail.com>
2024-04-26 11:57:10 -0300

    updated imports & example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:body_part_selector/body_part_selector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,29 +23,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends HookWidget {
   final String title;
+  //Fazer dois checkbox singulares, ao selecionar um deles, o texto e a imagem de exemplo muda, A imagem deve ser um exemplo de como captura, por exemplo: uma pessoa em TPose enquanto outra tira foto…
+  const MyHomePage({super.key, required this.title});
 
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<bool> _bodyParts = List.filled(9, false);
+  //load image & show points of interest
 
   @override
   Widget build(BuildContext context) {
+    final bodyParts = useState({
+      'MACRO_BP_FACE': false,
+      'MACRO_BP_NECK': false,
+      'MACRO_BP_UPPER_ARM': false,
+      'MACRO_BP_FOREARM': false,
+      'MACRO_BP_CHEST': false,
+      'MACRO_BP_ABDOMEN': false,
+      'MACRO_BP_UPPER_LEG': false,
+      'MACRO_BP_LOWER_LEG': false,
+      'MACRO_BP_HIP': false,
+      'MACRO_BP_BUTTOCKS': false,
+    });
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: SafeArea(
-        child: BodyPartSelectorTurnable(
-          onSelectionUpdated: (selection) {
-            setState(() {
-              // _bodyParts = selection;
+        child: MacroPartsSelectorTurnable(
+          bodyParts.value,
+          onSelectionStateUpdate: (selection) {
+            bodyParts.value = selection.map((key, value) {
+              return MapEntry(key, value);
             });
           },
           labelData: const RotationStageLabelData(
--- lib/body_part_selector.dart
+++ lib/body_part_selector.dart
@@ -1,4 +1,4 @@
-export 'src/body_part_selector.dart';
-export 'src/body_part_selector_turnable.dart';
+export 'src/macro_part_selector.dart';
+export 'src/macro_part_selector_turnable.dart';
 export 'src/model/body_parts.dart';
 export 'src/model/body_side.dart';
